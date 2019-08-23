e9f3d90e2232c92e1d89930e8e06f6577db5c088
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-23 00:37:46 +0000

    refractor code

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -3,8 +3,23 @@ require 'rails_helper'
 RSpec.describe GramsController, type: :controller do
 
   describe "grams#destroy action" do
+
+    it "shouldnt let the user who didnt create it destroy it" do 
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      delete :destroy, params: { id: gram.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "shouldnt let anautheinticated users detroy a gram" do 
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to new_user_session_path
+    end
     it 'successfully destroy gram in the database' do
       gram = FactoryBot.create(:gram)
+      sign_in gram.user
       delete :destroy, params: { id: gram.id }
       expect(response).to redirect_to root_path
       gram = Gram.find_by_id(gram.id)
@@ -12,28 +27,54 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it 'should return 404 if gram not found' do
+      user = FactoryBot.create(:user)
+      sign_in user
       delete :destroy, params: { id: 'spaceduck'}
       expect(response).to have_http_status(:not_found)
     end
   end
 
   describe 'grams#update action' do
+
+    it "shouldnt let users who didnt create gram update it " do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      patch :update, params: { id: gram.id, gram: { message: "yes"} }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'shouldnt let unauthenicated users update a gram ' do
+      gram = FactoryBot.create(:gram)
+      patch :update, params: { id: gram.id, gram: {message: "Hello!"} }
+      expect(response).to redirect_to new_user_session_path
+    end
+
+
     it 'successfully update grams' do
       gram = FactoryBot.create(:gram, message: 'Initial Value')
+      sign_in gram.user
+
       patch :update, params: { id: gram.id, gram: {message: 'Changed!'}}
+      
+
       expect(response).to redirect_to root_path
       gram.reload
       expect(gram.message).to eq 'Changed!'
     end
 
     it 'display 404 if gram isnt found' do 
+      user = FactoryBot.create(:user)
+      sign_in user
       patch :update, params: { id: 'YOLOSWAG', gram: {message: 'Changed'} }
       expect(response).to have_http_status(:not_found)
     end
 
     it 'if errors-render new form' do 
       gram = FactoryBot.create(:gram, message: 'Initial Value')
+      sign_in gram.user
       patch :update, params: { id: gram.id, gram: { message: ''}}
+      
       expect(response).to have_http_status(:unprocessable_entity)
       gram.reload
       expect(gram.message).to eq 'Initial Value'
@@ -41,14 +82,33 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#edit action" do
+
+    it "shouldnt let a user who did not create the gram to update it" do 
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "shouldn't let unauthenticated users edit a gram" do
+      gram = FactoryBot.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to redirect_to new_user_session_path
+    end
+
+
     it "should show the edit form if the gram is found" do
       gram = FactoryBot.create(:gram)
+      sign_in gram.user
       get :edit, params: { id: gram.id }
       expect(response).to have_http_status(:success)
     end
 
 
     it "return a 404 if gram is not found" do
+      user = FactoryBot.create(:user)
+      sign_in user
       get :edit, params: { id: 'TACOCAT'}
       expect(response).to have_http_status(:not_found)
     end
@@ -77,6 +137,7 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#new action" do
+
     it 'should require a user to be logged in' do
       post :create, params: { gram: { message: "Hello!"} }
       expect(response).to redirect_to new_user_session_path
