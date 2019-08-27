22706e4c95621a82e268c73b1efee415af9ba193
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-27 00:04:11 +0000

    finished comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -18,14 +18,18 @@ RSpec.describe CommentsController, type: :controller do
 
     it "should require users to be logged in before commenting on grams " do
       gram = FactoryBot.create(:gram)
+
       post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram'} }
+      
       expect(response).to redirect_to new_user_session_path
     end
 
     it "should return http status code if gram is not found" do
       user = FactoryBot.create(:user)
       sign_in user
+
       post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+
       expect(response).to have_http_status :not_found
     end
   end
